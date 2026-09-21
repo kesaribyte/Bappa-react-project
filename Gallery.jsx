78445d247f4b7c84bd43385baef05d58266eec9a
kesaribyte <shitoleomraje12@gmail.com>
2026-09-21 17:44:56 +0530

    gallery add to export img

---
+++ Gallery.jsx
@@ -0,0 +1,62 @@
+function Gallery() {
+    return (
+      <section className="px-10 py-16 bg-orange-50">
+  
+        <div className="max-w-6xl mx-auto">
+  
+          <div className="text-center mb-10">
+  
+            <p className="text-orange-500 font-semibold tracking-widest">
+              MOMENTS
+            </p>
+  
+            <h2 className="text-4xl font-bold mt-2">
+              Pune in Every Frame
+            </h2>
+  
+            <p className="text-gray-600 mt-3">
+              A glimpse of Ganeshotsav across Pune.
+            </p>
+  
+          </div>
+  
+          <div className="grid grid-cols-4 gap-5">
+  
+            <div className="h-64 bg-orange-200 rounded-2xl"></div>
+  
+            <div className="h-64 bg-orange-100 rounded-2xl"></div>
+  
+            <div className="h-64 bg-orange-200 rounded-2xl"></div>
+  
+            <div className="h-64 bg-orange-100 rounded-2xl"></div>
+
+
+            <div className="h-64 bg-orange-200 rounded-2xl"></div>
+
+            <div className="h-64 bg-orange-100 rounded-2xl"></div>
+
+            <div className="h-64 bg-orange-200 rounded-2xl"></div>
+
+            <div className="h-64 bg-orange-100 rounded-2xl"></div>
+            
+            <div className="h-64 bg-orange-200 rounded-2xl"></div>
+
+            <div className="h-64 bg-orange-100 rounded-2xl"></div>
+
+            <div className="h-64 bg-orange-200 rounded-2xl"></div>
+
+            <div className="h-64 bg-orange-100 rounded-2xl"></div>
+
+
+
+
+
+          </div>
+  
+        </div>
+  
+      </section>
+    )
+  }
+  
+  export default Gallery
